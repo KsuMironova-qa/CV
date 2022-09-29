2367f6bb65c428468280faee9cd336bac868d351
Ksusha <lsun4ik1994@gmail.com>
2022-09-29 21:19:33 +0300

    commit_29

--- js.js
+++
@@ -1,7 +0,0 @@
-/* Это объявление переменной, мы наши кнопку по тегу */
-const button = document.querySelector('button');
-
-/* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
-button.addEventListener('click', function() {
-	alert('Credo: The only way to do great work is to love what you do');
-})
